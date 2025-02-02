2f2693659ccbed89cdfc4bb2e9be32ec32f383b6
belodrepo <sxbravo@gmail.com>
2025-02-02 19:51:16 +0100

    Első commit.

---
+++ client.js
@@ -0,0 +1,74 @@
+
+const apiUrl = 'http://localhost:3000/api/users';
+const usersData = document.getElementById('usersData');
+
+async function getUsers() {
+    //debugger;
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+
+    
+    usersData.innerHTML = users.map(user => `
+        <tr>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.city}</td>
+            <td>${user.address}</td>
+            <td>${user.phone}</td>
+            <td>${user.email}</td>
+            <td>${user.gender}</td>
+            <td>
+            <button onClick="deleteUser(${user.id})">Törlés</button>
+            </td>
+        </tr>
+        `).join('');
+
+}
+
+document.getElementById('userForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    //debugger;
+    try {
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    if (!data.firstName || !data.lastName || !data.city || !data.address || !data.phone || !data.email|| !data.gender) {
+        alert('Hiányzó adatok!');
+    } else {
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+    const result = await response.json();
+
+    if (response.ok) {
+        alert(result.message);
+        getUsers();
+    } else {
+        alert(result.message);
+    }
+    e.target.reset();
+}
+}
+catch(e) {
+    alert(e.message);
+}
+})
+
+async function deleteUser(id) {
+    if (confirm('Valóban törölni akarod a felhasználót?')) {
+        const response = await fetch(`${apiUrl}/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            alert('A felhasználó törlése megtörtént.');
+            getUsers();
+        } else {
+            alert('A felhasználó adatainak a törlése sikertelen volt!');
+        }
+    }
+}
+
+getUsers();
---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const db = new sqlite3.Database('test.db', (err) => {
+if (err) {
+    console.log(err.message);
+} else {
+    console.log('Az adatbázis kapcsolat létrejött');
+}
+});
+
+db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    firstName TEXT NOT NULL,
+    lastName TEXT NOT NULL,
+    city TEXT NOT NULL,
+    address TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    email TEXT NOT NULL,
+    gender TEXT NOT NULL
+)`);
+
+app.post('/api/users', (req, res) => {
+    const { firstName, lastName, city, address, phone, email, gender } = req.body;
+
+    db.run(`INSERT INTO users (firstName, lastName, city, address, phone, email, gender) VALUES (?, ?, ?, ?, ?, ?, ?)`, 
+        [firstName, lastName, city, address, phone, email, gender], 
+        function (err) {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ message: 'Hiba történt az adatbok rögzítése során!' });
+            } else {
+                return res.status(201).send({ message: 'Az adatok rögzítése sikeres volt.', id: this.lastID, firstName, lastName, address, phone, email, gender });
+            }
+        });
+
+});
+
+app.get('/api/users', (req, res) => {
+    db.all(`SELECT * FROM users`, [], (err, records) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Hiba történt az adatok kiolvasása során' });
+        } 
+        res.status(200).json(records);
+        
+    });
+});
+
+app.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.run(`DELETE FROM users WHERE id = ?`, [id], 
+        function (err) {
+            if (err) {
+                return res.status(500).send(err.message);
+            } else {
+                res.status(204).json( { message: 'Sikeres adattörlés!' });
+            }
+
+        });
+});
+
+
+app.listen(port, () => {
+    console.log(`A szerver fut a ${port} számú porton`);
+})
